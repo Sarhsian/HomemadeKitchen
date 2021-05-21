03e4c522dc72405e7a1da4436ffef07cdb284503
sargsyan.mikhail.2017 <sargsyan.mikhail.2017@gmail.com>
2021-05-21 12:26:32 +0300

    updated email service

--- HomemadeKitchen/EmailService.cs
+++ HomemadeKitchen/EmailService.cs
@@ -1,8 +1,8 @@
-﻿using MailKit.Net.Smtp;
-using MimeKit;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Threading.Tasks;
 
 namespace CasualShop
@@ -11,25 +11,37 @@ namespace CasualShop
     {
         public void SendEmail(string email, string subject, string message)
         {
-            var emailMessage = new MimeMessage();
-            email = "sargsyan.mikhail.2017@gmail.com";
-            emailMessage.From.Add(new MailboxAddress("Site Manager", email));
-            emailMessage.To.Add(new MailboxAddress("", email));
-            emailMessage.Subject = subject;
-            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
-            {
-                Text = message
-            };
+            MailAddress from = new MailAddress("homemadekitchen2022@gmail.com", "Site Manager");
+            MailAddress to = new MailAddress("homemadekitchen2022@gmail.com");
+            MailMessage m = new MailMessage(from, to);
+            m.Subject = subject;
+            m.Body = message;
+            m.IsBodyHtml = true;
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+            smtp.Credentials = new NetworkCredential("homemadekitchen2022@gmail.com", "apppass123");
+            smtp.EnableSsl = true;
+            smtp.Send(m);
 
-            using (var client = new SmtpClient())
-            {
-                //await client.ConnectAsync("smtp.yandex.ru", 25, false);
-                client.Connect("smtp.gmail.com", 465, true);
-                client.Authenticate(email, "");
-                client.Send(emailMessage);
+            //var emailMessage = new MimeMessage();
+            //email = "sargsyan.mikhail.2017@gmail.com";
 
-                client.Disconnect(true);
-            }
+            //emailMessage.From.Add(new MailboxAddress("Site Manager", email));
+            //emailMessage.To.Add(new MailboxAddress("", email));
+            //emailMessage.Subject = subject;
+            //emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+            //{
+            //    Text = message
+            //};
+
+            //using (var client = new SmtpClient())
+            //{
+            //    //await client.ConnectAsync("smtp.yandex.ru", 25, false);
+            //    client.Connect("smtp.gmail.com", 587, true);
+            //    client.Authenticate(email, "SARgsYan9mikhail820157161514131211W7");
+            //    client.Send(emailMessage);
+
+            //    client.Disconnect(true);
+            //}
         }
     }
 }
